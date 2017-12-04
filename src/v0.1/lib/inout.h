7b6cf8a2d8726f0378ddb7b149715a83d38e133f
giacomozanatta <giacomo12596@gmail.com>
2017-12-04 11:44:36 +0100

    deleted old version

--- src/v0.1/lib/inout.h
+++
@@ -1,9 +0,0 @@
-#ifndef INOUT_H
-#define INOUT_H
-
-#include "listcoord.h"
-#include "campo.h"
-int scrivi_su_file(matrice campo, int righe, int colonne);
-int leggi_da_file(coordpila *lista_bombe, int* righe, int *colonne, int *bombe);
-void stampa_lista(coordpila lista_bombe);
-#endif
--- src/v0.1/main.c
+++
@@ -1,8 +0,0 @@
-#include "lib/gioco.h"
-#include <stdlib.h>
-#include <time.h>
-int main(){
-	srand(time(NULL));
-	gioca();
-	return 0;
-}
